30bc8860ec58429b9e64b7fc4638e7a25051970d
vk3503 <101197555+vkCreation3503@users.noreply.github.com>
2022-06-12 22:03:06 +0530

    Update stats.js

--- SleepTracker/stats.js
+++ SleepTracker/stats.js
@@ -1,3 +1,12 @@
+
+const hamburger = document.querySelector(".hamburger") ; 
+const sidebar = document.querySelector(".sidebar") ; 
+
+hamburger.addEventListener("click",()=>{
+    hamburger.classList.toggle("active"); 
+    sidebar.classList.toggle("active") ; 
+})
+
 var labels = [
     'Sunday',
     'Monday',
