363e4b7aee0137646fc70c0f4cb7ec88f62b76dd
DaveVaishnavi <99636505+DaveVaishnavi@users.noreply.github.com>
2022-07-09 14:58:36 +0530

    Add files via upload

---
+++ registration.php
@@ -0,0 +1,26 @@
+<?php
+session_start();
+header('location:login.php');
+$con=mysqli_connect('localhost','root');
+if($con){
+  echo "Connection established successfully !";
+}
+else{
+  echo "Could not establish connection with database !";
+}
+mysqli_select_db($con,'sleeptracker');
+$name=$_POST['name'];
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+$q="select * from signin where fullname='$name' && password='$password'&& email='$email'";
+$result=mysqli_query($con,$q);
+$num=mysqli_num_rows($result);
+if($num==1){
+  echo "Duplicate session detected !";
+}
+else{
+  $qy="insert into signin(fullname,email,password) values ('$name','$email,'$password')";
+  mysqli_query($con,$qy);
+}
+?>
